fd698032136d5ce8986bf881b5fcda478e63f416
Gary <gzhang1999@gmail.com>
2019-07-11 15:53:43 -0700

    Initial commit

---
+++ src/main/java/se/gzhang/wms/cloud/WmsCloudApplication.java
@@ -0,0 +1,13 @@
+package se.gzhang.wms.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmsCloudApplication.class, args);
+	}
+
+}
